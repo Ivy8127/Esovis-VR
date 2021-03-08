026110b0e64a4fc5c80a9dfe48801f839ca1565d
Ivy Mboya <mboyaivy@gmail.com>
2021-03-08 15:50:16 +0300

    Added historical sites

--- VRApp/urls.py
+++ VRApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),   
 	path('shop/', views.shop, name="shop"),
+	path('historical_sites/', views.historicalSites, name="historical_sites"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
 	path('update_item/', views.updateItem, name="update_item"),
--- VRApp/views.py
+++ VRApp/views.py
@@ -9,8 +9,30 @@ from .models import *
 
 
 def home(request):
-    return render(request, 'VRApp/home.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete =False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items =[]   
+        order ={'get_cart_total':0, 'get_cart_items': 0,'shipping': False} 
+        cartItems = order['get_cart_items'] 
+    context ={'items':items,'order':order,'cartItems': cartItems  }
+    return render(request, 'VRApp/home.html',context)
 
+def historicalSites(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete =False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items =[]   
+        order ={'get_cart_total':0, 'get_cart_items': 0,'shipping': False} 
+        cartItems = order['get_cart_items'] 
+    context ={'items':items,'order':order,'cartItems': cartItems  }
+    return render(request, 'VRApp/historical_sites.html',context)
 
 def shop(request):
 
